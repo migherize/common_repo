60c5524933d32417d5705a5c488cb7baf0e8dfd5
migherize <migherize@gmail.com>
2022-07-29 00:23:26 -0400

    clase Animal para el uso de objeto

--- commons/schemas.py
+++ commons/schemas.py
@@ -11,3 +11,17 @@ class Persona:
     def hablar(self, mensaje):
         """Mostrar mensaje de saludo de Persona"""
         return mensaje
+
+class Animal:
+    """Clase que representa una Animal"""
+    
+    def __init__(self, dueño, nombre, tipo, sexo):
+        """Constructor de clase Persona"""
+        self.dueño = dueño
+        self.nombre = nombre
+        self.tipo = tipo
+        self.sexo = sexo
+
+    def Come(self, mensaje):
+        """Mostrar mensaje de saludo de Persona"""
+        return mensaje
